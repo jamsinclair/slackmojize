b3eb684c18bec8cfc39da03852a81f175247afd4
jamsinclair <jamsinclairnz@gmail.com>
2016-07-27 01:57:45 +1200

    Also proxy image HEAD request so client can get past CORS

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var request = require('request');
 var app = express();
-
+var resizeApiUrl = 'http://img-resize.com/resize';
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/web'));
 
 app.post('/resize', function(req, resp) {
-  req.pipe(request('http://img-resize.com/resize')).pipe(resp);
+  req.pipe(request(resizeApiUrl)).pipe(resp);
+})
+
+app.head('/filesize', function(req, resp) {
+  var imageUrl = req.query.url;
+
+  if (imageUrl) {
+    req.pipe(request(imageUrl)).pipe(resp);
+  } else {
+    resp.sendStatus('404');
+  }
 })
 
 app.listen(app.get('port'), function() {
