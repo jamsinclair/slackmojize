cacf4bb88bdba1be1305bad17f3d4aeb90bf4927
jamsinclair <jamsinclairnz@gmail.com>
2016-08-28 15:28:53 +1200

    Add some comments for the endpoints

--- index.js
+++ index.js
@@ -7,10 +7,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/web'));
 
+// Proxy all resize requests to the API URL, doing this to get around CORS
 app.post('/resize', function(req, resp) {
   req.pipe(request(resizeApiUrl)).pipe(resp);
 })
 
+// Hacky endpoint to provide an easy way to get the content length of images for the Client.
 app.head('/filesize', function(req, resp) {
   var imageUrl = req.query.url;
 
