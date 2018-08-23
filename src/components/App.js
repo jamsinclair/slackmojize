a2909847ef44201f9bc6466b65c44b99376234a9
jamsinclair <jamsinclairnz@gmail.com>
2018-08-23 13:38:15 +0900

    Update App template to match original template

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import DropZone from './DropZone'
 import './App.css';
 
 class App extends Component {
+  state = {
+    files: []
+  }
+
   render() {
     return (
       <div className="App">
-        <header>
-          <h1>Slackmojize</h1>
+        <header className="App-header">
+          <h1 className="App-title">Slackmojize</h1>
           <h2>
-            <img src="./parrot.gif" alt="Parrot"/>
-            Easily resize images in your browser for Slack Emojis
-            <img src="./rightparrot.gif" alt="Right Parrot"/>
+            <img className="App-parrot-left" src="./parrot.gif" alt="Parrot"/>
+            Easily resize images for Slack Emojis
+            <img className="App-parrot-right" src="./rightparrot.gif" alt="Right Parrot"/>
           </h2>
         </header>
+        <DropZone></DropZone>
+        <footer className="App-footer">
+          Hacked together by <a href="https://github.com/jamsinclair/slackmojize">jamsinclair</a>
+        </footer>
       </div>
     );
   }
