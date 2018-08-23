897179fd41e36ed3cff0c7353002198bf9feb5b5
jamsinclair <jamsinclairnz@gmail.com>
2018-08-23 14:09:06 +0900

    Add EmojiZone component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import DropZone from './DropZone'
+import EmojiZone from './EmojiZone'
 import './App.css';
 
 class App extends Component {
@@ -14,11 +15,14 @@ class App extends Component {
           <h1 className="App-title">Slackmojize</h1>
           <h2>
             <img className="App-parrot-left" src="./parrot.gif" alt="Parrot"/>
-            Easily resize images for Slack Emojis
+            Easily resize images for Slack&nbsp;Emojis
             <img className="App-parrot-right" src="./rightparrot.gif" alt="Right Parrot"/>
           </h2>
         </header>
-        <DropZone></DropZone>
+        <div className="App-body">
+          <DropZone></DropZone>
+          <EmojiZone></EmojiZone>
+        </div>
         <footer className="App-footer">
           Hacked together by <a href="https://github.com/jamsinclair/slackmojize">jamsinclair</a>
         </footer>
---
+++ src/components/EmojiZone.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './EmojiZone.css';
+
+class EmojiZone extends Component {
+  render() {
+    return (
+      <div className="EmojiZone">
+        EmojiZone
+      </div>
+    );
+  }
+}
+
+export default EmojiZone;
