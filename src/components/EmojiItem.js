19a6025334b7901455bde5fc237f07c744689459
jamsinclair <jamsinclairnz@gmail.com>
2018-08-28 15:25:04 +0900

    Refactor Emoji Item components - Moves actions into own componets, hides download when error and renders error message

--- src/components/EmojiItem.js
+++ src/components/EmojiItem.js
@@ -23,6 +23,39 @@ const renderEmojiImage = image => {
   return <img className="EmojiItem-image" src={image.resizedDataUri} alt={image.original.name} />
 }
 
+const ErrorMessage = ({ error }) => {
+  if (!error) {
+    return null
+  }
+
+  return <span className="EmojiItem-name-error">Error Resizing: </span>
+}
+
+const RemoveAction = ({removeImage, imageKey}) => (
+  <span
+    className="EmojiItem-action EmojiItem-action--remove"
+    onClick={() => removeImage(imageKey)}
+    >
+    <FontAwesomeIcon icon={faTrashAlt} size="1x" />
+  </span>
+)
+
+const DownloadAction = ({ image }) => {
+  if (image.error) {
+    return null
+  }
+
+  return (
+    <a
+      className="EmojiItem-action EmojiItem-action--download"
+      download={image.original.data.name}
+      href={image.resizedDataUri}
+      >
+      <FontAwesomeIcon icon={faDownload} size="1x" />
+    </a>
+  )
+}
+
 const EmojiItem = ({ image, imageKey, removeImage }) => {
   const isLoading = !image.resizedDataUri && !image.error
 
@@ -30,19 +63,12 @@ const EmojiItem = ({ image, imageKey, removeImage }) => {
     <li className={`EmojiItem ${ isLoading ? 'EmojiItem--loading' : '' }`}>
       {renderEmojiImage(image)}
       <div className="EmojiItem-name">
+        <ErrorMessage error={image.error} />
         {image.original.data.name}
       </div>
       <div className="EmojiItem-actions">
-        <a
-          className="EmojiItem-action EmojiItem-action--download"
-          download={image.original.data.name}
-          href={image.resizedDataUri}
-          >
-          <FontAwesomeIcon icon={faDownload} size="1x" />
-        </a>
-        <span className="EmojiItem-action EmojiItem-action--remove" onClick={() => removeImage(imageKey)}>
-          <FontAwesomeIcon icon={faTrashAlt} size="1x" />
-        </span>
+        <DownloadAction image={image} />
+        <RemoveAction removeImage={removeImage} imageKey={imageKey} />
       </div>
     </li>
   )
